21eb80ea71a6e5b1c39e78c7e28e140efd0b625b
lain561 <mckissicethan@gmail.com>
2024-12-19 16:38:15 -0500

    fixed styling

--- library/inputform.js
+++ library/inputform.js
@@ -3,19 +3,22 @@ const myLibrary = [];
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookPages = document.getElementById('pages'); 
+const bookRead   = document.getElementById('pages'); 
 
 document.querySelector('.submit').addEventListener('click', addBookToLibrary);
 
-function Book(title, author, pages){
+function Book(title, author, pages, read){
     this.title = title;
     this.author = author; 
     this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(){
-    myLibrary.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value));
-
-    console.log(myLibrary[0].bookTitle);
-    console.log(myLibrary[0].author);
-    console.log(myLibrary[0].pages);
+    const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
+    myLibrary.push(newBook);
+    console.log(myLibrary[0]); 
+    console.log(myLibrary[0]); 
 }
+
+
