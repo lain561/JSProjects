9cd868555f48f3be9f30b44d64a797bf04ccc84c
ethan mckissic <mckissicethan@gmail.com>
2025-02-05 20:30:21 -0500

    integrated module encapsulation

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -1,54 +1,87 @@
-// Object to control flow of game
-const Game = {
-    "gameBoard": [],
-    "gameResult": false,
-    "playerTurn": 1
-}
 
-// Start new game
-const player1 = new Player(1);
-const player2 = new Player(2);
+const gameTiles = document.querySelector(".game-tiles"); // container to store the game tiles
 
-const gameBoard = document.querySelector(".game-tiles"); // container to store the game tiles
-const playText = document.querySelector(".game-turn"); // displays player 1/2's turn
+const Game = (() => {
+    let playerTurn = 1; 
+    let gameResult = false; 
+
+    // Player factory function
+    const createPlayer = (name, value) => ({ name, value }); 
+    
+    let player1 = createPlayer("Player 1", "X");
+    let player2 = createPlayer("Player 2", "O");
+
+    const startButton = document.querySelector(".start"); // starts game and renders board
+    startButton.addEventListener("click", (event) => {
+        const player1Name = document.getElementById("player1").value;
+        const player2Name = document.getElementById("player2").value;
+        start(player1Name, player2Name);
+        event.preventDefault();
+    }); 
+
+    const start = (player1Name, player2Name) => {
+        if (player1Name && player2Name){
+            player1 = createPlayer(player1Name, "X" );
+            player2 = createPlayer(player2Name, "O");
+            console.log(player1);
+            console.log(player2);
+        }
+        
+
+        // BEFORE STARTING A NEW GAME, CHECK IF BOARD WAS ALREADY RENDERED
+        gameBoard.renderBoard();   
+    };
+
+    const playText = document.querySelector(".game-turn"); // displays player 1/2's turn
+
+    // Inserts X/O accordingly based on turn
+    const insertMove = (tile) => {
+        // If tile has not been used
+        if(tile.innerHTML == ""){
+            // / Player 1's turn
+            if (playerTurn === 1){
+                tile.innerHTML = player1.value;
+                playText.innerHTML = `${player2.name}'s Turn`;
+                playerTurn = 2;
+            }
+            // Player 2's turn
+            else if (playerTurn === 2){
+                tile.innerHTML = player2.value;
+                playText.innerHTML = `${player1.name}'s Turn`; 
+                playerTurn = 1;
+            }        
+        }
+    }
+
+    return { insertMove, playerTurn }
+})(); 
+
+const gameBoard = (() => {
+    let board = new Array(9); // create board with 9 tiles 
+
+    // FOR TILES REMEMBER TO ADD THE HOVER EVENT LISTENDER 
+
+    // Renders gameboard tiles onto the DOM
+    const renderBoard = () => {
+        for(let i = 0; i < 9; i++){
+            const tile = document.createElement("button"); 
+            tile.classList.add("tile"); // add appropriate styling
+            tile.id = `square-${i}`; // assign id for future indexing
+            tile.addEventListener("click", () => Game.insertMove(tile)); // create event-listener for player's move
+            gameTiles.appendChild(tile); //insert into grid
+        }
+    };
+
+    return { renderBoard }; // return function as object 
+})();
+
+
 const restartButton = document.querySelector(".restart"); // button to clear the game board
 restartButton.addEventListener("click", (event) => {clearGame(event)}); 
-createTiles(); // create tic-tac-toe grid
+
 const tiles = document.querySelectorAll(".tile"); //gets list of tiles for the game
 
-function Player(name){
-    this.name = name;
-}
-
-// Creates the 3x3 grid for the game
-function createTiles(){
-    for(let i = 0; i < 9; i++){
-        const tile = document.createElement("button"); 
-        tile.classList.add("tile"); // add appropriate styling
-        tile.addEventListener("click", () => insertMove(tile)); // create event-listener for player's move
-        gameBoard.appendChild(tile); //insert into grid
-    }
-}
-
-// Inserts X/O accordingly based on turn
-function insertMove(tile){
-    // If tile has not been used
-    if(tile.innerHTML == ""){
-        // / Player 1's turn
-        if (Game.playerTurn === 1){
-            tile.innerHTML = "X";
-            playText.innerHTML = "Player 2's Turn";
-            Game.playerTurn = 2;
-        }
-        // Player 2's turn
-        else if (Game.playerTurn === 2){
-            tile.innerHTML = "O";
-            playText.innerHTML = "Player 1's Turn";
-            Game.playerTurn = 1;
-        }        
-    }
-}
-
+// clear gameboard
 function clearGame(event){   
     // For each tile in the grid
     tiles.forEach((tile) => {
