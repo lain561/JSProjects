2ea6b4c852d26322091409e210e42c7c6103604b
ethan mckissic <mckissicethan@gmail.com>
2025-02-10 02:05:34 -0500

    finished, can add more comments if not lazy......

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -1,4 +1,15 @@
 const Game = (() => {
+    const winCombos = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
     let playerTurn = 1; 
     let gameResult = false; 
 
@@ -14,6 +25,7 @@ const Game = (() => {
         const player1Name = document.getElementById("player1").value;
         const player2Name = document.getElementById("player2").value;
         start(player1Name, player2Name);
+        gameResult = false;
         event.preventDefault();
     }); 
 
@@ -29,27 +41,8 @@ const Game = (() => {
             gameBoard.clearBoard();
         }
 
-        playText.innerHTML = `${player1.name}'s Turn`;
+        playText.innerHTML = `${player1.name}'s Turn (X)`;
     };
-
-    // Inserts X/O accordingly based on turn
-    const insertMove = (tile) => {
-        // If tile has not been used
-        if(!tile.innerHTML){
-            // / Player 1's turn
-            if (playerTurn === 1){
-                tile.innerHTML = player1.value;
-                playText.innerHTML = `${player2.name}'s Turn`;
-                playerTurn = 2;
-            }
-            // Player 2's turn
-            else if (playerTurn === 2){
-                tile.innerHTML = player2.value;
-                playText.innerHTML = `${player1.name}'s Turn`; 
-                playerTurn = 1;
-            }        
-        }
-    }
 
     // Button to reset the game board
     const restartButton = document.querySelector(".restart"); 
@@ -58,17 +51,79 @@ const Game = (() => {
         playerTurn = 1;
 
         if(playText.innerHTML){
-            playText.innerHTML = `${player1.name}'s Turn`;
+            playText.innerHTML = `${player1.name}'s Turn (X)`;
         }
 
         event.preventDefault(); // prevent form from submitting
+        gameResult = false;
     }); 
+
+     // Inserts X/O accordingly based on turn
+     const insertMove = (tile) => {
+        //check if game is ongoing
+        if(!gameResult){
+            // If tile has not been used
+            if(!tile.innerHTML){
+                const index = tile.id.split('-')[1];;
+                
+                // / Player 1's turn
+                if(playerTurn === 1){
+                    gameBoard.setBoard(index, "X");
+                    tile.innerHTML = player1.value;
+                    playText.innerHTML = `${player2.name}'s Turn (O)`;
+                    checkWin(); 
+                    playerTurn = 2;
+                }
+                // Player 2's turn
+                else if(playerTurn === 2){
+                    gameBoard.setBoard(index, "O");
+                    tile.innerHTML = player2.value;
+                    playText.innerHTML = `${player1.name}'s Turn (X)`; 
+                    checkWin(); 
+                    playerTurn = 1; 
+                }       
+            }
+        }
+    }
+
+    const checkWin = () => {
+        board = gameBoard.getBoard();
+        console.log(board);
+
+        for(let i = 0; i < winCombos.length; i++){
+            combo = winCombos[i];
+            cell1 = board[combo[0]];
+            cell2 = board[combo[1]];
+            cell3 = board[combo[2]];
+
+            if(cell1 === "" && cell2 === "" && cell3 === ""){
+                continue;
+            }
+
+            if((cell1 === cell2) && (cell2 === cell3)){
+                gameResult = true; 
+                break;
+            }
+        }
+
+        if(gameResult){
+            if(playerTurn === 1){
+                playText.innerHTML = `${player1.name} (X) WINS!`
+            } else{
+                playText.innerHTML = `${player2.name} (O) WINS!`
+            }
+        }
+
+        else if(!board.includes("")){
+            playText.innerHTML = `DRAW!`
+        }
+    };
 
     return { insertMove }
 })(); 
 
 const gameBoard = (() => {
-    let board = new Array(9); // create board with 9 tiles 
+    let board = ["", "", "", "", "", "", "", "", ""]; // create board with 9 tiles 
 
     const gameTiles = document.querySelector(".game-tiles"); // container to store the game tiles
 
@@ -77,7 +132,7 @@ const gameBoard = (() => {
         for(let i = 0; i < 9; i++){
             const tile = document.createElement("button"); 
             tile.classList.add("tile"); // add appropriate styling
-            tile.id = `square${i}`; // assign id for future indexing
+            tile.id = `square-${i}`; // assign id for future indexing
             tile.addEventListener("click", () => Game.insertMove(tile)); // create event-listener for player's move
             gameTiles.appendChild(tile); // insert into grid
         }
@@ -91,11 +146,21 @@ const gameBoard = (() => {
         tiles.forEach((tile) => {
             tile.innerHTML = ""; // clear current game
         });
+
+        board = ["", "", "", "", "", "", "", "", ""];
     };
     
     const checkBoard = () => {
         return gameTiles.innerHTML;
     };  
 
-    return { renderBoard, clearBoard, checkBoard }; // return function as object 
+    const setBoard = (index, symbol) => {
+        board[index] = symbol;
+    };
+
+    const getBoard = () => {
+        return board;
+    };
+
+    return { renderBoard, clearBoard, checkBoard, setBoard, getBoard }; // return function as object 
 })();
