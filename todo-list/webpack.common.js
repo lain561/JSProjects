f294794ee06ebf0e73165681b1f4837a9a543992
ethan mckissic <mckissicethan@gmail.com>
2025-03-05 02:33:27 -0500

    setup react practice project

--- todo-list/webpack.common.js
+++ todo-list/webpack.common.js
@@ -3,7 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: "./src/js/App.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
